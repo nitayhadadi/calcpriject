54a9bd9576772c620202381d92f1ccc84118624e
nitay <nitay@DESKTOP-UP7TDND>
2023-12-04 12:09:51 +0200

    Add project files.

---
+++ calcpriject/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace calcpriject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class GamePage : Window
+    {
+        public GamePage()
+        {
+            InitializeComponent();
+        }
+         internal class gamepage
+        {
+        
+    }
+        private void SubmitButton_Click(object sender, RoutedEventArgs e)
+        {
+            string name = NameTextBox.Text;
+            string age = AgeTextBox.Text;
+            GamePage gamepage = new GamePage();
+            this.Visibility = Visibility.Hidden;
+            gamepage.Show();
+
+        }
+
+    }
+
+   
+}
---
+++ calcpriject/Window3.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace calcpriject
+{
+    /// <summary>
+    /// Interaction logic for Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+
+        private int Number1;
+        private int Number2;
+        private int Correctanswer;
+
+        // Constructor
+        public Window2()
+        {
+            InitializeComponent(); // This is necessary to initialize the window components
+        }
+
+        public void game(int gamekind)
+        {
+            Random random = new Random();
+            Number1 = random.Next(1, 10); 
+            Number2 = random.Next(1, 10);
+        }
+        public double add(double number1, double number2)
+        {
+            return number1 + number2;
+        }
+
+      
+
+        public double subtract(double number1, double number2)
+        {
+            return number1 - number2;
+        }
+
+        public double multiply(double number1, double number2)
+        {
+            return number1 * number2;
+        }
+
+        public double divide(double number1, double number2)
+        {
+            if (number2 == 0)
+            {
+                throw new DivideByZeroException("Cannot divide by zero.");
+            }
+            return number1 / number2;
+
+        }
+        
+
+
+    }
+        }
